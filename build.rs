858fdc9fbd7172687fb1adf05f75283ffb4dd320
Brandon T. P. Saint-John <saint-john@lbl.gov>
2025-04-22 22:20:46 -0700

    Fill in more of handlegraph extern

--- build.rs
+++ build.rs
@@ -1,10 +1,28 @@
-fn main() {
-    cxx_build::bridge("src/lib.rs")
-        .include("libhandlegraph/src/include")
-        .file("libhandlegraph/src/types.cpp")
-        .std("c++14")
-        .compile("cxx-handlegraph");
-    println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=libhandlegraph/src/types.cpp");
-    println!("cargo:rerun-if-changed=libhandlegraph/src/util.cpp");
+use std::path::PathBuf;
+
+fn main() -> miette::Result<()> {
+    // let includes = [
+    //     PathBuf::from("libhandlegraph/src/include"),
+    //     // "libhandlegraph/src/include/handlegraph",
+    //     // "libhandlegraph/src/include/handlegraph/util",
+    //     // "libhandlegraph/src/include/handlegraph/types",
+    // ];
+    // let mut builder = autocxx_build::Builder::new("src/lib.rs", &includes)
+    //     .build()?;
+        
+    // builder
+    //     .file("libhandlegraph/src/types.cpp")
+    //     .flag_if_supported("-std=c++14")
+    //     .compile("cxx-handlegraph");
+    // println!("cargo:rerun-if-changed=src/lib.rs");
+    // Ok(())
+    // cxx_build::bridge("src/lib.rs")
+    //     .include("libhandlegraph/src/include")
+    //     .file("libhandlegraph/src/types.cpp")
+    //     .std("c++14")
+    //     .compile("cxx-handlegraph");
+    // println!("cargo:rerun-if-changed=src/lib.rs");
+    // println!("cargo:rerun-if-changed=libhandlegraph/src/types.cpp");
+    // println!("cargo:rerun-if-changed=libhandlegraph/src/util.cpp");
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,107 @@
-#[cxx::bridge(namespace = "handlegraph")]
-pub mod ffi {
-    unsafe extern "C++" {
-        include!("cxx-handlegraph/libhandlegraph/src/include/handlegraph/types.hpp");
-        include!("cxx-handlegraph/libhandlegraph/src/include/handlegraph/util.hpp");
-        pub type handle_t;
+use autocxx::prelude::*;
 
-        pub fn as_handle(value: &u64) -> &handle_t;
+// include_cpp! {
+//     #include "handlegraph/types.hpp"
+//     #include "handlegraph/util.hpp"
+//     #include "handlegraph/handle_graph.hpp"
+//     #include "handlegraph/mutable_handle_graph.hpp"
+//     // safety!(safe)
+//     generate!("handlegraph::handle_t")
+//     generate!("handlegraph::nid_t")
+//     generate!("handlegraph::as_handle")
+//     generate!("handlegraph::HandleGraph")
+//     generate!("handlegraph::MutableHandleGraph")
+
+// }
+use handlegraph::{
+    handle::NodeId,
+    hashgraph::{HashGraph, Node},
+    pathhandlegraph::{MutableGraphPaths, PathId},
+};
+use ref_cast::RefCast;
+
+struct RustHashGraph(HashGraph);
+
+#[derive(RefCast)]
+#[repr(transparent)]
+struct RustNode(Node);
+
+fn new_node(sequence: &[u8]) -> Box<RustNode> {
+    Box::new(RustNode(Node::new(sequence)))
+}
+
+impl RustHashGraph {
+    fn print_occurences(&self) {
+        self.0.print_occurrences();
+    }
+
+    fn get_node(&self, id: &ffi::RustNodeId) -> Result<*const RustNode, String> {
+        self.0
+            .get_node(&NodeId(id.inner))
+            .map(|n| {
+                let n = RustNode::ref_cast(n);
+                let n: *const RustNode = n;
+                n
+            })
+            .ok_or(String::from("Node not found"))
+    }
+
+    fn print_path(&self, path_id: &ffi::RustPathId) {
+        self.0.print_path(&PathId(path_id.inner));
+    }
+
+    fn create_path(&mut self, name: &[u8], circular: bool) -> Result<ffi::RustPathId, String> {
+        self.0
+            .create_path(name, circular)
+            .map(|path_id| ffi::RustPathId { inner: path_id.0 })
+            .ok_or("Path creation failed".to_string())
+    }
+}
+
+fn new_hash_graph() -> Box<RustHashGraph> {
+    Box::new(RustHashGraph(HashGraph::new()))
+}
+
+#[cxx::bridge]
+pub mod ffi {
+    struct RustNodeId {
+        inner: u64,
+    }
+
+    struct RustPathId {
+        inner: u64,
+    }
+    extern "Rust" {
+        type RustNode;
+    }
+    extern "Rust" {
+        type RustHashGraph;
+        fn new_hash_graph() -> Box<RustHashGraph>;
+        fn print_occurences(&self);
+        fn get_node(&self, node_id: &RustNodeId) -> Result<*const RustNode>;
+        fn print_path(&self, path_id: &RustPathId);
+        fn create_path(&mut self, name: &[u8], circular: bool) -> Result<RustPathId>;
     }
 }
 
 #[cfg(test)]
 mod test {
+    use std::pin::pin;
+
+    // use crate::ffi::handlegraph::{HandleGraph, MutableHandleGraph};
+
     use super::*;
 
-    #[test]
-    fn test_handle_t() {
-        let value: u64 = 42;
-        let handle: &ffi::handle_t = ffi::as_handle(&value);
-        // assert_eq!(handle, &value);
-    }
+    // #[test]
+    // fn test_handle_t() {
+    //     let value = pin!(42);
+    //     let handle = unsafe { ffi::handlegraph::as_handle(value) };
+    // }
+
+    // #[test]
+    // fn test_handle_t() {
+    //     let value: u64 = 42;
+    //     let handle: &ffi::handle_t = ffi::as_handle(&value);
+    //     // assert_eq!(handle, &value);
+    // }
 }
