97d304cfda7a0a030c98f70c7f8b8a8aced6e536
Brandon Saint-John <bsaintjo@ucsc.edu>
2025-04-23 06:43:14 -0700

    Start cpp side

--- build.rs
+++ build.rs
@@ -16,12 +16,12 @@ fn main() -> miette::Result<()> {
     //     .compile("cxx-handlegraph");
     // println!("cargo:rerun-if-changed=src/lib.rs");
     // Ok(())
-    // cxx_build::bridge("src/lib.rs")
+    cxx_build::bridge("src/lib.rs")
     //     .include("libhandlegraph/src/include")
     //     .file("libhandlegraph/src/types.cpp")
-    //     .std("c++14")
-    //     .compile("cxx-handlegraph");
-    // println!("cargo:rerun-if-changed=src/lib.rs");
+        .std("c++14")
+        .compile("cxx_handlegraph");
+    println!("cargo:rerun-if-changed=src/lib.rs");
     // println!("cargo:rerun-if-changed=libhandlegraph/src/types.cpp");
     // println!("cargo:rerun-if-changed=libhandlegraph/src/util.cpp");
     Ok(())
---
+++ cpp/src/implementation.cpp
@@ -0,0 +1,7 @@
+#include <handlegraph/util.hpp>
+#include <rust_handle_graph/implementation.hpp>
+
+namespace rust_handlegraph {
+    using namespace handlegraph;
+    
+}
